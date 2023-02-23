2d15fa96d0ed9ac488d98e9945d8413830f745d7
Mohamed Ahmed <91838581+3ab2wy1911@users.noreply.github.com>
2023-02-24 01:02:10 +0200

    Update main.java

--- Game in Java/main.java
+++ Game in Java/main.java
@@ -1,17 +1,51 @@
 import javax.management.monitor.GaugeMonitor;
 import java.util.Scanner;
+abstract class Board {
+    protected
+    int n;
+    char grid[][] = new char [n][n];
 
-class player
+    public
+
+
+    abstract boolean update_board(int x,char symbol);
+
+    abstract boolean is_winner(char symbol);
+    abstract boolean is_draw(char symbol1,char symbol2) ;
+    abstract void display_board();
+}
+abstract class Player
+{
+    protected
+    String name;
+    char symbol;
+    public
+   abstract char get_symbol();
+    abstract String get_name();
+
+}
+abstract class Game {
+    private
+    XO_Board board;
+    int turn;
+
+    public
+
+    abstract void play();
+}
+
+class XO_Player extends Player
 {
     protected
         String name;
         char symbol;
     public
-    player(String n, char s)    // Assigning the name and the Symbol of the player.
+    XO_Player(String n, char s)    // Assigning the name and the Symbol of the player.
     {
         name = n;
         symbol = s;
     }
+    public
     char get_symbol()
     {
         return symbol;
@@ -22,13 +56,14 @@ class player
     }   //Returning the name.
 
 }
-class Board
+class XO_Board extends Board
 {
     protected
     char [][] grid = new char[3][3];
     public
-    Board()
+    XO_Board()
     {
+        n =3;
         char x='1';
         for(int i=0;i<3;i++)
         {
@@ -39,14 +74,16 @@ class Board
             }
         }
     }
-    Boolean in_range (int input)    // Function to prevent User entering inputs out of range.
+    public
+    boolean in_range (int input)    // Function to prevent User entering inputs out of range.
     {
         if (input<10 & input>0 )
             return true;
         System.out.println("Out of Range!!!");
         return false;
     }
-    Boolean update_board (int input, char symbol)   //Function that updates the grid if the input is correct otherwise it will retake the input again.
+    public
+    boolean update_board(int input, char symbol)   //Function that updates the grid if the input is correct otherwise it will retake the input again.
     {
         for(int i =0 ;i<3;i++)
         {
@@ -62,7 +99,8 @@ class Board
         System.out.println("Wrong move ,please enter again:");
         return false;
     }
-    Boolean is_winner (char symbol)    // Check if there is a winner
+    public
+    boolean is_winner (char symbol)    // Check if there is a winner
     {
         for (int i=0;i < 3;i++) {
             int c=0;
@@ -116,7 +154,8 @@ class Board
         }
         return false;
     }
-    Boolean is_Draw(char symbol1, char symbol2) //Check if it's draw.
+    public
+    boolean is_draw(char symbol1, char symbol2) //Check if it's draw.
     {
         for(int i= 0 ;i<3;i++)
         {
@@ -129,7 +168,8 @@ class Board
         }
         return (!is_winner(symbol1) & !is_winner(symbol2));
     }
-    void print_board()  // Printing the board.
+    public
+    void display_board()  // Printing the board.
     {
         for (int i=0;i<3;i++)
         {
@@ -142,15 +182,15 @@ class Board
         }
     }
 }
-class game
+class XO_Game extends Game
 {
     private
-    Board board = new Board();
+    XO_Board board = new XO_Board();
     int turn =0 ;
     public
-    game()
+    XO_Game()
     {};
-
+public
     void play()
     {
         // Taking players names and assigning their symbols.
@@ -158,14 +198,14 @@ class game
         String name;
         Scanner input1 = new Scanner(System.in);
         name = input1.nextLine();
-        player p1 = new player(name,'X');
+        XO_Player p1 = new XO_Player(name,'X');
         System.out.print("Enter Second player name:");
         Scanner input2 = new Scanner(System.in);
         name = input2.nextLine();
-        player p2 = new player(name,'O');
-        board.print_board();
+        XO_Player p2 = new XO_Player(name,'O');
+        board.display_board();
         // Condition to check is the game still in progress or not.
-        while (!board.is_Draw(p1.get_symbol(),p2.get_symbol()) & !(board.is_winner(p1.get_symbol()) || board.is_winner(p2.get_symbol())))
+        while (!board.is_draw(p1.get_symbol(),p2.get_symbol()) & !(board.is_winner(p1.get_symbol()) || board.is_winner(p2.get_symbol())))
         {
             int input;
             if (turn ==0) {
@@ -179,7 +219,7 @@ class game
                     Scanner input5 = new Scanner(System.in);
                     input = input5.nextInt();
                 }
-                board.print_board();
+                board.display_board();
                 turn = 1;
             }
             else if (turn == 1)
@@ -194,7 +234,7 @@ class game
                     Scanner input5 = new Scanner(System.in);
                     input = input5.nextInt();
                 }
-                board.print_board();
+                board.display_board();
                 turn = 0;
             }
         }
@@ -211,7 +251,7 @@ class game
 
 public class main{
     public static void main(String[] args) {
-        game Game = new game();
-        Game.play();
+        XO_Game game = new XO_Game();
+        game.play();
     }
 }
